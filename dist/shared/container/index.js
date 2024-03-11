d2fabcd9975954c5e713d078fee6cd2a6a404d9d
italo <italovieira.008@gmail.com>
2024-03-11 10:21:35 -0300

    add dist in gitignore

--- dist/shared/container/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tsyringe_1 = require("tsyringe");
-const UserRepository_1 = require("../../modules/usuarios/infra/repositories/UserRepository");
-tsyringe_1.container.registerSingleton("UserRepository", UserRepository_1.UserRepository);
-//# sourceMappingURL=index.js.map
--- dist/shared/infra/http/routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const User_routes_1 = require("../../../../modules/usuarios/routes/User.routes");
-const router = (0, express_1.Router)();
-exports.router = router;
-router.use("/user", User_routes_1.userRoute);
-//# sourceMappingURL=index.routes.js.map
